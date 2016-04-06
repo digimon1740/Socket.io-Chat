2479a2cb916a94ae2f21770d6751608c4692e6b9
devsh <digimon.1740@gmail.com>
2016-04-06 23:53:17 +0900

    bootstrap 기반 채팅방 입장 페이지 추가

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+var path = require('path');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/admission', function(req,res) {
+	res.sendFile(__dirname + '/public/room.html');
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 var nicknames = [];
 
